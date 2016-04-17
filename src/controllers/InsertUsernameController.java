714ef4f4930fbdf7f331096dd873dc820cd0007a
ViktorGorchev <viktor_g_g@yahoo.com>
2016-04-17 16:47:43 +0300

    Username validation message.

--- src/controllers/InsertUsernameController.java
+++ src/controllers/InsertUsernameController.java
@@ -7,6 +7,7 @@ import interfaces.Player;
 import interfaces.StageManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -17,9 +18,14 @@ import java.util.regex.Pattern;
 
 
 public class InsertUsernameController extends AbstractController {
-    public TextField usernameTextField;
+    @FXML
+    private TextField usernameTextField;
 
-    public Button enterUsernameButton;
+    @FXML
+    private Button enterUsernameButton;
+
+    @FXML
+    private Label usernameInfo;
 
     @Override
     public void setStageManager(StageManager stageManager) {
@@ -40,14 +46,26 @@ public class InsertUsernameController extends AbstractController {
         }
     }
 
+    @FXML
     private void setPlayer() {
         String input = this.usernameTextField.getText();
-        Pattern pattern = Pattern.compile("([a-zA-Z]+)[\\W]+([0-9]+)");
-        String name = input;
-        Player gamePlayer = new GamePlayer(100, 100 ,name);
-        getStageManager().getDatabase().setPlayer(gamePlayer);
-        this.getStageManager().getDatabase().addHighScore(gamePlayer.getHighScore());
-        setNextScene();
+
+        Pattern validationPattern = Pattern.compile("[\\w]{5,16}");
+        Matcher validationMatcher = validationPattern.matcher(input);
+
+        String name = "";
+        if (validationMatcher.find()){
+            name = input;
+            Player gamePlayer = new GamePlayer(100, 100 ,name);
+
+            this.getStageManager().getDatabase().setPlayer(gamePlayer);
+            this.getStageManager().getDatabase().addHighScore(gamePlayer.getHighScore());
+            setNextScene();
+
+        } else{
+            this.usernameTextField.clear();
+            this.usernameInfo.setVisible(true);
+        }
     }
 
     private void setNextScene() {
